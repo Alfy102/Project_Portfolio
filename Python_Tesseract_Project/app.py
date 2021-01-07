7891e8356fc32885ea09c7f416d0396ea13eb222
Alfy102 <aliff102.syazwan@gmail.com>
2021-01-07 14:35:55 +0800

    Fix the issue with uploaded image not displayed correctly

--- Python_Tesseract_Project/app.py
+++ Python_Tesseract_Project/app.py
@@ -4,23 +4,20 @@ import webbrowser
 from os.path import join, dirname, realpath
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-# import our OCR function
-from ocr_core import ocr_core
+from ocr_core import ocr_core #import OCR engine
 
-# define a folder to store and later serve the images
-UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'static/uploads/')
-CALL_FOLDER = '/static/uploads/'
-# allow files of a specific type
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+CALL_FOLDER = "static/uploads/" #path from app.py
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), CALL_FOLDER) #path from /home...
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 #max file size of 1MB
+app.config['UPLOAD_EXTENSIONS'] = ['png', 'jpg', 'jpeg'] #allowed extension
 
-webbrowser.open('http://127.0.0.1:5000/', new=2)
-# function to check the file extension
+webbrowser.open('http://localhost:8081/', new=2)
+
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['UPLOAD_EXTENSIONS'] # check uploaded file extension
 
 # route and function to handle the home page
 @app.route('/')
@@ -38,20 +35,22 @@ def upload_page():
         # if no file is selected
         if file.filename == '':
             return render_template('upload.html', msg='No file selected')
-
+        #if file is selected, check with allowable extension for valid file
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # call the OCR function on it
+            folder_path = (os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(folder_path)
+            #replace spacing on filename to underscore as it can causes err 404 in img_src
+            new_filename = filename.replace(' ', '_')
+            os.rename(folder_path,(os.path.join(app.config['UPLOAD_FOLDER'], new_filename)))
+            # pass the image to OCR engine
             extracted_text = ocr_core(file)
-
-            # extract the text and display it
+            # pass the extracted text back to html.
             return render_template('upload.html',
                                    msg='Successfully processed',
                                    extracted_text=extracted_text,
-                                   img_src=CALL_FOLDER + file.filename)
+                                   img_src=CALL_FOLDER + new_filename)
     elif request.method == 'GET':
         return render_template('upload.html')
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=False)
