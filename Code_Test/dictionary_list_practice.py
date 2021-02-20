16ffe53677bb30e52b7b0e77abc061bb0c7dcba0
Alfy102 <aliff102.syazwan@gmail.com>
2021-02-20 23:26:39 +0800

    updated to show old way

--- Code_Test/dictionary_list_practice.py
+++ Code_Test/dictionary_list_practice.py
@@ -1,24 +1,53 @@
 import csv
 path = '/home/koopla102/Portfolio/Project_Portfolio/Code_Test/'
-country_list=[]
-country_dict={'Country':[],'Capital':[]}
+country_list_old=[]
+capital_list_old=[]
+country_dict_old={'Country':[],'Capital':[]}
+country_dict_new={}
+country_list_new=[]
 
 with open(path + 'Country_and_Capitals.csv', mode='r') as data:
     for j,line in enumerate(csv.DictReader(data)):
         locals().update(line)
-        country_list.append(line)
-        country_dict[j] = {"Country":Country,"Capital":Capital}
+        country_dict_old['Country'].append(Country)
+        country_dict_old['Capital'].append(Capital)
+        country_list_old.append(Country)
+        capital_list_old.append(Capital)
+        country_list_new.append(line)
+        country_dict_new[j] = {'Country':Country, 'Capital':Capital}
 
 i=14 #set pointer value
 
-#--------------------------------Direct print for list----------------------------------------
 
-#print('Country = ' + country_list[i] + '\nCapital = ' + country_list[i])
-country_list_info = country_list[i]
+#-----------Iterating through Dictionary using two for loops (when there is no numbering)----------
+if i >= (len(country_dict_old['Country'])-1):
+    print("out of bound")
+
+for count1, element in enumerate(country_dict_old['Country']):
+    if count1==i:
+        print('Country = ' + element)
+
+for count2, element in enumerate(country_dict_old['Capital']):
+    if count2==i:
+        print('Capital = ' + element)
+
+print('Country = ' + country_list_old[i] + '\nCapital = ' + capital_list_old[i])
+
+#--------------------------------------------------------------------------------------------------
+
+
+#--------------------------------The better and smarter way----------------------------------------
+
+country_list_info = country_list_new[i]
 print(country_list_info)
 print('Country = ' + country_list_info['Country'] + '\nCapital = ' + country_list_info['Capital'])
 
-#--------------------------------direct print for dictionary------------------------------
-country_dict_info = country_dict.get(i)
+country_dict_info = country_dict_new.get(i)
 print(country_dict_info)
 print('Country = ' + country_dict_info['Country'] + '\nCapital = ' + country_dict_info['Capital'])
+
+
+
+print(country_dict_old)
+
+print(country_dict_new)
